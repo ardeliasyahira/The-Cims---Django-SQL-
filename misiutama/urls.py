107867d455b530704889dcc003923128457430f4
Aiko Sulaiman <aiko.sulaiman@ui.ac.id>
2022-05-15 02:36:03 +0700

    tambah fitur read misi utama pemain

--- misiutama/urls.py
+++ misiutama/urls.py
@@ -6,5 +6,6 @@ app_name = 'misiutama'
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('admin_read_misiutama/', views.admin_read_misiutama, name='admin_read_misiutama')
+    path('admin_read_misiutama/', views.admin_read_misiutama, name='admin_read_misiutama'),
+    path('pemain_read_misiutama/', views.pemain_read_misiutama, name='pemain_read_misiutama')
 ]
--- misiutama/views.py
+++ misiutama/views.py
@@ -15,3 +15,16 @@ def admin_read_misiutama(request):
     data['list_misiutama'] = list_misiutama
 
     return render(request, 'admin_read_misiutama.html', data)
+
+def pemain_read_misiutama(request):
+    if not is_authenticated(request):
+        return login(request)
+    if request.session['role'] == 'admin':
+        return HttpResponse("Anda bukanlah pemain")
+
+    list_misiutama = query("SELECT * FROM MISI_UTAMA")
+
+    data = get_session_data(request)
+    data['list_misiutama'] = list_misiutama
+
+    return render(request, 'pemain_read_misiutama.html', data)
