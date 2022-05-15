b691db150002f34b30b2acc38c521626946479d9
Aiko Sulaiman <aiko.sulaiman@ui.ac.id>
2022-05-15 13:24:18 +0700

    adding detail misi utama & update navbar

--- misiutama/urls.py
+++ misiutama/urls.py
@@ -7,5 +7,7 @@ app_name = 'misiutama'
 urlpatterns = [
     path('', views.login, name='login'),
     path('admin_read_misiutama/', views.admin_read_misiutama, name='admin_read_misiutama'),
-    path('pemain_read_misiutama/', views.pemain_read_misiutama, name='pemain_read_misiutama')
+    path('admin_detail_misiutama/<str:nama>', views.admin_detail_misiutama, name='admin_detail_misiutama'),
+    path('pemain_read_misiutama/', views.pemain_read_misiutama, name='pemain_read_misiutama'),
+    path('pemain_detail_misiutama/<str:nama>', views.pemain_detail_misiutama, name='pemain_detail_misiutama'),
 ]
--- misiutama/views.py
+++ misiutama/views.py
@@ -28,3 +28,29 @@ def pemain_read_misiutama(request):
     data['list_misiutama'] = list_misiutama
 
     return render(request, 'pemain_read_misiutama.html', data)
+
+def pemain_detail_misiutama(request, nama):
+    if not is_authenticated(request):
+        return login(request)
+    if request.session['role'] == 'admin':
+        return HttpResponse("Anda bukanlah pemain")
+
+    list_detailmisi = query(f"SELECT * FROM MISI WHERE nama = '{nama}'")
+
+    data = get_session_data(request)
+    data['list_detailmisi'] = list_detailmisi
+
+    return render(request, 'detail_read_misiutama.html', data)
+
+def admin_detail_misiutama(request, nama):
+    if not is_authenticated(request):
+        return login(request)
+    if request.session['role'] == 'pemain':
+        return HttpResponse("Anda bukanlah admin")
+
+    list_detailmisi = query(f"SELECT * FROM MISI WHERE nama = '{nama}'")
+
+    data = get_session_data(request)
+    data['list_detailmisi'] = list_detailmisi
+
+    return render(request, 'detail_read_misiutama.html', data)
